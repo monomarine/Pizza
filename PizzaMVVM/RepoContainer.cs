2b0b1752e6026a66c2c7aa375fd5851a73ec88d3
Elena <egurnovaelena56@gmail.com>
2025-04-10 11:39:35 +0500

    добавлен список клиентов Vew

---
+++ PizzaMVVM/RepoContainer.cs
@@ -0,0 +1,23 @@
+﻿using PizzaMVVM.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Unity;
+using Unity.Lifetime;
+
+namespace PizzaMVVM
+{
+    internal static class RepoContainer
+    {
+        private static IUnityContainer _container;
+        static RepoContainer()
+        {
+            _container = new UnityContainer();
+            _container.RegisterType<ICustomerRepository, CustomerRepository>(
+                new ContainerControlledLifetimeManager() );
+        }
+        public static IUnityContainer Container => _container;
+    }
+}
--- PizzaMVVM/ViewModels/CustomerViewModel.cs
+++ PizzaMVVM/ViewModels/CustomerViewModel.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using PizzaMVVM.Models;
+using PizzaMVVM.Repositories;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Linq;
 using System.Text;
@@ -9,10 +12,83 @@ namespace PizzaMVVM.ViewModels
 {
     internal class CustomersViewModel : BaseViewModel
     {
-        private string fullName;
-        public string FullName { 
-            get => fullName;
-            set => SetProperty(ref fullName, value, nameof(FullName));   
+        private ICustomerRepository _customerRepository;
+        private ObservableCollection<Customer> _customers;
+        public ObservableCollection<Customer> Customers
+        {
+            get => _customers;
+            set => SetProperty(ref _customers, value);
         }
+
+        public CustomersViewModel()
+        {
+            _customerRepository = new CustomerRepository();
+            LoadCustomers();
+
+            EditCustomerCommand = new RelayCommand<Customer>(OnEditCustomer);
+            AddCustomerCommand = new RelayCommand(OnAddCustomer);
+            PlaceNewOrderCommand = new RelayCommand<Customer>(OnPlaceOrder);
+            ClearSearchInputCommand = new RelayCommand(ClearSearch);
+        }
+
+        private List<Customer> _customerList;
+        private async Task LoadCustomers()
+        {
+            _customerList = await _customerRepository.GetCustomersAsync();
+            Customers = new ObservableCollection<Customer>(_customerList);
+            
+        }
+
+        //поиск клиентов
+        private string _searchInput;
+        public string SearchInput
+        {
+            get => _searchInput;
+            set
+            {
+                SetProperty(ref _searchInput, value);
+                FindCustomerByName(_searchInput);
+            }
+        }
+
+        public void FindCustomerByName(string text)
+        {
+            if (string.IsNullOrEmpty(text))
+            {
+                Customers = new ObservableCollection<Customer>(_customerList);
+                return;
+            }
+            else
+            {
+                Customers = new ObservableCollection<Customer>(
+                    _customerList.Where(x => x.FullName.ToLower()
+                    .Contains(text.ToLower())));
+            }
+        }
+
+        public RelayCommand AddCustomerCommand { get; private set; }
+        public RelayCommand<Customer> EditCustomerCommand { get; private set; }
+        public RelayCommand ClearSearchInputCommand { get; private set; }  
+        public RelayCommand<Customer> PlaceNewOrderCommand { get; private set; }
+
+        public event Action<Guid> PlaceOrderRequested;
+        public event Action<Customer> AddNewCustomerRequested;
+        public event Action<Customer> EditCustomerRequested;
+
+        private void OnAddCustomer() => 
+            AddNewCustomerRequested?.Invoke(new Customer() { Id = new Guid() });
+
+        private void OnEditCustomer(Customer customer) =>
+            EditCustomerRequested?.Invoke(customer);
+
+        private void ClearSearch() =>
+            SearchInput = null;
+
+        private void OnPlaceOrder(Customer customer) =>
+            PlaceOrderRequested.Invoke(customer.Id);
+
+
+
+
     }
 }
